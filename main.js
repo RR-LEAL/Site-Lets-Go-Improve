37f41ed5fd997c66647bc301b218dd6ba465f77c
Rodrigo Rodrigues Leal da Silva <rr.leal4@gmail.com>
2021-07-08 09:40:18 -0300

    Conclusão de 2 seções
    Concluímos as seções Clientes e Contatos

--- main.js
+++ main.js
@@ -9,10 +9,50 @@ for (const element of toggle) {
   })
 }
 
-// quando clicar em um item do menu, esconder o menu */
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links)
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
+
+/* mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/* Clients carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  sliderPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #astartup .imagestartup, #astartup .textstartup, #astartup .imagemiq, #astartup .textmiq,
+  #services header, #services .card,
+  #clients header, #clients .clients,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
